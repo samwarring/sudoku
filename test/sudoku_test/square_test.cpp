1487b3c60beb8707ddf3ac7a3b20ae971ca92e8a
samwarring <samwarring@gmail.com>
2020-01-15 10:57:46 -0800

    One more unit-test for 16x16 square formatter
    16x16 is the first square sudoku to use double-digit placeholders. This unit-test ensure everything aligns correctly in that situation

--- test/sudoku_test/square_test.cpp
+++ test/sudoku_test/square_test.cpp
@@ -52,3 +52,34 @@ BOOST_AUTO_TEST_CASE(SquareFormatter_Root3)
     std::string actual = fmt.format(cellValues);
     BOOST_REQUIRE_EQUAL(expected, actual);
 }
+
+BOOST_AUTO_TEST_CASE(SquareFormatter_Root4)
+{
+    // The first square with double-digit placeholders.
+        sudoku::square::Dimensions dims(4);
+    sudoku::square::Formatter fmt(dims);
+    std::vector<size_t> cellValues(256, 0);
+    std::string expected = 
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        "------------+-------------+-------------+------------\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        "------------+-------------+-------------+------------\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        "------------+-------------+-------------+------------\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0\n"
+        " 0  0  0  0 |  0  0  0  0 |  0  0  0  0 |  0  0  0  0";
+    std::string actual = fmt.format(cellValues);
+    BOOST_REQUIRE_EQUAL(expected, actual);
+
+}
