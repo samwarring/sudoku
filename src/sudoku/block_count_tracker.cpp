b0a9893eca7d1badb0e9397f2961d6f7ee0fc572
samwarring <samwarring@gmail.com>
2020-01-29 21:34:52 -0800

    Added class sudoku::BlockCounter
    - Motivation is to obtain the maxBlockEmptyCell in O(1) time.
    - This will replace some (if not all) logic managed by the sudoku::Potential class.
    - Needs to be integrated with sudoku::Grid. Coming soon...
    - Added unit-tests

---
+++ src/sudoku/block_count_tracker.cpp
@@ -0,0 +1,125 @@
+#include <cassert>
+#include <numeric>
+#include <sudoku/block_count_tracker.h>
+
+namespace sudoku
+{
+    BlockCountTracker::BlockCountTracker(const sudoku::Dimensions& dims)
+        : occupiedBlockCount_(static_cast<int>(dims.getMaxCellValue() + 1))
+        , posIndex_(dims.getCellCount())
+        , posHeap_(dims.getCellCount() + 1)
+        , blockCounts_(dims.getCellCount())
+    {
+        // Don't use the first position in the heap.
+        // posHeap[1] is the _real_ max of the heap.
+        // This simplifies implementation.
+        std::iota(posIndex_.begin(), posIndex_.end(), 1);
+        std::iota(posHeap_.begin() + 1, posHeap_.end(), 0);
+        posHeap_[0] = static_cast<size_t>(~0);
+    }
+
+    void BlockCountTracker::incrementBlockCount(size_t cellPos)
+    {
+        assert(cellPos < posIndex_.size());
+        assert(blockCounts_[cellPos] != -1);
+        blockCounts_[cellPos]++;
+        onBlockCountIncrease(cellPos);
+    }
+
+    void BlockCountTracker::derementBlockCount(size_t cellPos)
+    {
+        assert(cellPos < posIndex_.size());
+        assert(blockCounts_[cellPos] != 0);
+        blockCounts_[cellPos]--;
+        onBlockCountDecrease(cellPos);
+    }
+
+    void BlockCountTracker::markCellOccupied(size_t cellPos)
+    {
+        assert(cellPos < posIndex_.size());
+        assert(blockCounts_[cellPos] >= 0);
+        blockCounts_[cellPos] -= occupiedBlockCount_;
+        onBlockCountDecrease(cellPos);
+    }
+
+    void BlockCountTracker::markCellEmpty(size_t cellPos)
+    {
+        assert(cellPos < posIndex_.size());
+        assert(blockCounts_[cellPos] < 0);
+        blockCounts_[cellPos] += occupiedBlockCount_;
+        onBlockCountIncrease(cellPos);
+    }
+
+    size_t BlockCountTracker::getLeftHeapChild(size_t heapIndex)
+    {
+        return heapIndex * 2;
+    }
+
+    size_t BlockCountTracker::getRightHeapChild(size_t heapIndex)
+    {
+        return (heapIndex * 2) + 1;
+    }
+
+    size_t BlockCountTracker::getHeapParent(size_t heapIndex)
+    {
+        return heapIndex / 2;
+    }
+
+    void BlockCountTracker::onBlockCountIncrease(size_t cellPos)
+    {
+        size_t heapIndex = posIndex_[cellPos];
+        size_t parentIndex = getHeapParent(heapIndex);
+        while (parentIndex && trySwap(heapIndex, parentIndex)) {
+            heapIndex = parentIndex;
+            parentIndex = getHeapParent(parentIndex);
+        }
+    }
+
+    void BlockCountTracker::onBlockCountDecrease(size_t cellPos)
+    {
+        size_t heapIndex = posIndex_[cellPos];
+        size_t leftChildIndex = getLeftHeapChild(heapIndex);
+        size_t rightChildIndex = getRightHeapChild(heapIndex);
+        for(;;) {
+            if (leftChildIndex >= posHeap_.size()) {
+                // No children. Don't swap anything.
+                return;
+            }
+            else if (rightChildIndex >= posHeap_.size()) {
+                // Only 1 left child. Attempt one swap and return.
+                trySwap(leftChildIndex, heapIndex);
+                return;
+            }
+            else if (trySwap(leftChildIndex, heapIndex)) {
+                // left child was swapped.
+                heapIndex = leftChildIndex;
+                leftChildIndex = getLeftHeapChild(heapIndex);
+                rightChildIndex = getRightHeapChild(heapIndex);
+                continue;
+            }
+            else if (trySwap(rightChildIndex, heapIndex)) {
+                // right child was swapped.
+                heapIndex = rightChildIndex;
+                leftChildIndex = getLeftHeapChild(heapIndex);
+                rightChildIndex = getRightHeapChild(heapIndex);
+                continue;
+            }
+            // No children, or nothing was swapped.
+            return;
+        }
+    }
+
+    bool BlockCountTracker::trySwap(size_t childIndex, size_t parentIndex)
+    {
+        auto childCellPos = posHeap_[childIndex];
+        auto parentCellPos = posHeap_[parentIndex];
+        if (blockCounts_[parentCellPos] < blockCounts_[childCellPos]) {
+            posHeap_[parentIndex] = childCellPos;
+            posHeap_[childIndex] = parentCellPos;
+            posIndex_[parentCellPos] = childIndex;
+            posIndex_[childCellPos] = parentIndex;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ test/sudoku_test/block_count_tracker_test.cpp
@@ -0,0 +1,74 @@
+#include <boost/test/unit_test.hpp>
+#include <sudoku/block_count_tracker.h>
+#include <sudoku/square.h>
+
+void alterBlockCount(sudoku::BlockCountTracker& tracker, size_t cellPos, int delta)
+{
+    if (delta > 0) {
+        for (int i = 0; i < delta; ++i) {
+            tracker.incrementBlockCount(cellPos);
+        }
+    }
+    else {
+        for (int i = 0; i < (-delta); ++i) {
+            tracker.derementBlockCount(cellPos);
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE(BlockCountTracker_incrementBlockCount)
+{
+    sudoku::square::Dimensions dims(2);
+    sudoku::BlockCountTracker tracker(dims);
+
+    tracker.incrementBlockCount(4);  // blockCount(4)=1
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 4);
+
+    tracker.incrementBlockCount(10); // blockCount(4)=1, blockCount(10)=1
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 4);
+
+    tracker.incrementBlockCount(10); // blockCount(4)=1, blockCount(10)=2
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 10);
+
+    alterBlockCount(tracker, 0, 3);  // blockCount(0)=3, blockCount(4)=1, blockCount(10)=3
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(BlockCountTracker_decrementBlockCount)
+{
+    sudoku::square::Dimensions dims(2);
+    sudoku::BlockCountTracker tracker(dims);
+
+    alterBlockCount(tracker, 1, 4);
+    alterBlockCount(tracker, 2, 4);
+    alterBlockCount(tracker, 3, 4); // blockCount(1)=4, blockCount(2)=4, blockCount(3)=4
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 1);
+
+    tracker.derementBlockCount(1);
+    tracker.derementBlockCount(2);  // blockCount(1)=3, blockCount(2)=3, blockCount(3)=4
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 3);
+
+    alterBlockCount(tracker, 3, -2);
+    alterBlockCount(tracker, 1, -1); // blockCount(1)=2, blockCount(2)=3, blockCount(3)=2
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(BlockCountTracker_occupyAndEmptyCell)
+{
+    sudoku::square::Dimensions dims(2);
+    sudoku::BlockCountTracker tracker(dims);
+
+    alterBlockCount(tracker, 6, 3);
+    alterBlockCount(tracker, 7, 2);
+    alterBlockCount(tracker, 8, 1);
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 6);
+
+    tracker.markCellOccupied(6);
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 7);
+
+    tracker.markCellOccupied(7);
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 8);
+
+    tracker.markCellEmpty(6);
+    BOOST_REQUIRE_EQUAL(tracker.getMaxBlockEmptyCell(), 6);
+}
