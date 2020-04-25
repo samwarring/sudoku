c1e85f126a5973ebd4ca3240f83ac1714b967fe5
samwarring <samwarring@gmail.com>
2020-04-25 16:22:36 -0700

    Using gcovr may not be necessary

--- scripts/travis_after_success.sh
+++ scripts/travis_after_success.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
 if [ "$COVERAGE" = "ON" ]; then
-    echo "Generating coverage.xml"
-    gcovr -r .. . --xml-pretty > coverage.xml
     echo "Uploading coverage to codecov.io"
     bash <(curl -s https://codecov.io/bash)
 fi
